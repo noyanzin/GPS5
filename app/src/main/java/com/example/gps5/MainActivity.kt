5ea58f3e55851fbc2760781a4480857ce17f547f
noyanzin <noyanzin@inbox.ru>
2022-11-12 22:56:22 +1200

    Added LocationUpdate

--- app/src/main/java/com/example/gps5/MainActivity.kt
+++ app/src/main/java/com/example/gps5/MainActivity.kt
@@ -5,16 +5,35 @@ import android.content.pm.PackageManager
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import com.example.gps5.databinding.ActivityMainBinding
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
+    }
+    private lateinit var locationCallback: LocationCallback
+    private fun startLocationUpdates() {
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,8 +41,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY,0).build()
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                p0 ?: return
+                for(location in p0.locations){
+                    Log.i("flow","Update UI with locationResult data")
+                    show(location)
+                }
 
+            }
+        }
         binding.btnLast.setOnClickListener {
             Log.i("flow", "GetLastLocation")
             fetchLocation()
