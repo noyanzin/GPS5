569256016f6f5d6dd44bfab24f83cd1daa2e0bb8
noyanzin <noyanzin@inbox.ru>
2022-11-12 17:56:45 +1200

    Show last location

---
+++ app/src/main/java/com/example/gps5/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.gps5
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import com.example.gps5.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.i("flow", "onStart")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        binding.btnLast.setOnClickListener {
+            Log.i("flow", "GetLastLocation")
+            fetchLocation()
+        }
+
+    }
+    private fun fetchLocation(){
+        Log.i("flow","fetchLocation start")
+        val task = fusedLocationProviderClient.lastLocation
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 101)
+                Log.i("flow", "fetchLocation request permission")
+                return
+            }
+       task.addOnSuccessListener {
+           Log.i("flow","show")
+           binding.longatude.text = it?.longitude.toString()
+           binding.latitude.text = it?.latitude.toString()
+           binding.speed.text = it?.speed.toString()
+           //show(it)
+       }
+    }
+    private fun show(location: Location){
+        binding.longatude.text = location.longitude.toString()
+        binding.latitude.text = location.latitude.toString()
+        binding.speed.text = location.speed.toString()
+
+    }
+}
